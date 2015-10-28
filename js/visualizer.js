ec579573faaf89899256a3728a65918014993682
phiaseitz <sophia.seitz@students.olin.edu>
2015-10-27 21:24:13 -0400

    We have a map

--- js/visualizer.js
+++ js/visualizer.js
@@ -7,3 +7,46 @@
  */
 
 console.log("hello world")
+
+var width = 960,
+  height = 1160;
+
+// var svg = d3.select("body").append("svg")
+//   .attr("width", width)
+//   .attr("height", height);
+
+// d3.json("./js/europe-map.geo.json", function(error, europe) {
+//   if (error) return console.error(error);
+  
+//   // svg.append("path")
+//   //     .datum(topojson.feature(europe, europe.geometry.coordinates))
+//   //     .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+// });
+
+ //Define map projection
+var projection = d3.geo.mercator()
+                       .translate([width/2, height/2])
+                       .scale([500]);
+
+//Define path generator
+var path = d3.geo.path()
+                 .projection(projection);
+
+//Create SVG element
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+//Load in GeoJSON data
+d3.json("./js/europe-map.geo.json", function(json) {
+
+    //Bind data and create one path per GeoJSON feature
+    svg.selectAll("path")
+       .data(json.features)
+       .enter()
+       .append("path")
+       .attr("d", path)
+       .style("fill", "steelblue");
+
+});
