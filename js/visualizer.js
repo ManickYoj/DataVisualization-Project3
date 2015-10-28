4ff6edafdca33a1f2e191797bf426ad33086ab01
phiaseitz <sophia.seitz@students.olin.edu>
2015-10-27 21:29:25 -0400

    Got rid of the countries that aren't in the EU

--- js/visualizer.js
+++ js/visualizer.js
@@ -6,22 +6,10 @@
  *  creates the visualization.
  */
 
-console.log("hello world")
 
 var width = 960,
   height = 1160;
 
-// var svg = d3.select("body").append("svg")
-//   .attr("width", width)
-//   .attr("height", height);
-
-// d3.json("./js/europe-map.geo.json", function(error, europe) {
-//   if (error) return console.error(error);
-  
-//   // svg.append("path")
-//   //     .datum(topojson.feature(europe, europe.geometry.coordinates))
-//   //     .attr("d", d3.geo.path().projection(d3.geo.mercator()));
-// });
 
  //Define map projection
 var projection = d3.geo.mercator()
